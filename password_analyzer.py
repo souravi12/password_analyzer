94011d7c891d580d4b4dd59a375c988d57495d7e
souravi12 <souravidas121@gmail.com>
2026-06-20 23:59:00 +0530

    Updated project

--- password_analyzer.py
+++ password_analyzer.py
@@ -1,15 +1,14 @@
-import getpass
+from flask import Flask, render_template, request
 import string
 
+app = Flask(__name__)
 
-def check_pwd():
-    password = getpass.getpass("Enter your password: ")
-    score=0
-    #adding here ki how many are there ig?
-   
+# ---------- PASSWORD LOGIC ----------
+def analyze_password(password):
+    score = 0
     lower_count = upper_count = num_count = wspace_count = special_count = 0
-    for char in (password):
-        
+
+    for char in password:
         if char in string.ascii_lowercase:
             lower_count += 1
         elif char in string.ascii_uppercase:
@@ -20,58 +19,36 @@ def check_pwd():
             wspace_count += 1
         else:
             special_count += 1
-       
+
     if len(password) >= 8: score += 1
     if lower_count > 0: score += 1
     if upper_count > 0: score += 1
     if num_count > 0: score += 1
     if special_count > 0: score += 1
 
-    print("\nPassword check")
-    
-
-
-    if upper_count ==0:
-        print("hint: Add uppercase letters")
-    if lower_count ==0:
-        print("Add lowercase letters")
-    if num_count ==0:
-        print("Add numbers")                    
-    if special_count ==0:
-        print("Add special characters") 
-
-    if   score == 1: 
-        remarks = "Very Bad Password!!! Change ASAP"
-    elif score == 2:
-        remarks = "Not A Good Password!!! Change ASAP"
-    elif score ==3:
-        remarks = "It's a weak password, consider changing"
+    if score <= 3:
+        remarks = "Weak password"
     elif score == 4:
-        remarks = "It's a hard password, but can be better"
-    elif score == 5:
+        remarks = "Good password"
+    else:
+        remarks = "Very strong password"
 
-        remarks = "A very strong password"
- 
-    print(f"Hint: {remarks}")
+    return {
+        "score": score,
+        "remarks": remarks
+    }
 
-def ask_pwd(another_pwd=False):
-    while True:
-        if another_pwd:
-            choice=input('Do you want to enter another password (y/n):')    
-        else:
-            choice=input('Do you want to check your password (y/n):')
-        if choice.lower() == 'y':
-            check_pwd()
-            another_pwd=True
-        elif choice.lower() == 'n':
-            print('Thank you for using the password checker <3')
-            return False
-        else:
-            print('Do you want to try again?')
-if __name__ == '__main__':
-    print('welcome to PWD checker ^^')
+# ---------- ROUTE ----------
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = None
 
-    ask_pw = ask_pwd()
-    while ask_pw:
-        check_pwd()
-        ask_pw = ask_pwd(True)
+    if request.method == "POST":
+        password = request.form["password"]
+        result = analyze_password(password)
+
+    return render_template("index.html", result=result)
+
+# ---------- RUN ----------
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function myFunction() {
+  var input = document.getElementById("myInput");
+  var icon = document.querySelector(".eye i");
+
+  if (input.type === "password") {
+    input.type = "text";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
+  } else {
+    input.type = "password";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
+  }
+}
